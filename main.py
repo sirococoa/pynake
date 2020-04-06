f3cd4fb9aa203e30c88aca8f0fa10e06e4149dfb
sirococoa <torakia.mcml178@gmail.com>
2020-04-06 12:52:57 +0900

    result画面の調整

--- main.py
+++ main.py
@@ -165,11 +165,12 @@ class App:
     def draw(self):
         pyxel.cls(0)
         if App.game_over:
-            pyxel.text(center("GAMEOVER", WINDOW_SIZE)  + randint(-1, 1), WINDOW_SIZE // 2 + randint(-1, 1), "GAMEOVER", 10)
-            pyxel.text(center("GAMEOVER", WINDOW_SIZE), WINDOW_SIZE // 2, "GAMEOVER", 7)
+            pyxel.text(center("GAMEOVER", WINDOW_SIZE)  + randint(-1, 1), WINDOW_SIZE // 4 + randint(-1, 1), "GAMEOVER", 10)
+            pyxel.text(center("GAMEOVER", WINDOW_SIZE), WINDOW_SIZE // 4, "GAMEOVER", 7)
             message = ["SCORE:", str(self.snake1.length), "x", str(self.snake2.length), ">>", str(self.snake1.length*self.snake2.length)]
             message_color = [7, 5, 7, 10, 7, 7]
-            text_mc(center("".join(message), WINDOW_SIZE), WINDOW_SIZE // 4 * 3, message, message_color)
+            text_mc(center("".join(message), WINDOW_SIZE), WINDOW_SIZE // 2, message, message_color)
+            pyxel.text(center("<R> to restart", WINDOW_SIZE), WINDOW_SIZE // 4, "<R> to restart", 10)
         else:
             # pyxel.text(center(str(self.flame), WINDOW_SIZE), WINDOW_SIZE//2, str(self.flame), 7)
             self.snake1.draw()
